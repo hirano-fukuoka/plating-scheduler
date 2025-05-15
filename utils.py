08a5dfb2817f42e52b04a22469c9a708b867e065
hirano-fukuoka <58233549+hirano-fukuoka@users.noreply.github.com>
2025-05-15 15:15:47 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,23 @@
 import plotly.express as px
 import pandas as pd
 
-def plot_gantt(df):
-    df['StartTime'] = pd.to_datetime(df['Start'], unit='m', origin='2025-01-01')
-    df['EndTime'] = pd.to_datetime(df['End'], unit='m', origin='2025-01-01')
-    return px.timeline(df, x_start="StartTime", x_end="EndTime", y="JobID", color="PlatingType")
+def plot_gantt(df, start_date):
+    df['StartTime'] = pd.to_datetime(start_date) + pd.to_timedelta(df['Start'], unit='m')
+    df['EndTime'] = pd.to_datetime(start_date) + pd.to_timedelta(df['End'], unit='m')
+    fig = px.timeline(
+        df,
+        x_start="StartTime",
+        x_end="EndTime",
+        y="JobID",
+        color="PlatingType",
+        title="めっき工程スケジュール"
+    )
+    fig.update_layout(
+        xaxis=dict(
+            tickformat="%m/%d",
+            title="日付"
+        ),
+        yaxis_title="ジョブID",
+        margin=dict(l=20, r=20, t=50, b=20)
+    )
+    return fig
